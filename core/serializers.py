ab2afeaef2be2cda594b477c5fecbf829b4e4bfa
rafabrasail <rafaelrosenberg89@gmail.com>
2022-02-14 19:40:47 -0300

    commit03

---
+++ core/serializers.py
@@ -0,0 +1,18 @@
+from .models import List, Item
+from rest_framework import serializers
+
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['name', 'done', 'url']
+
+
+class ListSerializer(serializers.HyperlinkedModelSerializer):
+    item_set = ItemSerializer(many=True)
+
+    class Meta:
+        model = List
+        fields = ['name', 'owner', 'url', 'item_set']
+
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions, authentication
+from .serializers import ListSerializer, ItemSerializer
+from .models import List, Item
 
-# Create your views here.
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
+
+
+class ListViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
